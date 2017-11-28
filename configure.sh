60bcc7dc7e32e25537b4fe3b26f23cb78c690435
brianwrf <brianwangrewa@gmail.com>
2017-11-28 16:56:49 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,4 +1,8 @@
 mkdir /etc/openvpn
+cp server.conf /etc/openvpn
+cp client.conf /etc/openvpn
+cp mkvpnclient.sh /etc/openvpn
+cp mkinline.sh /etc/openvpn
 cp /tmp/vpn/openvpn-certs/server/easyrsa3/pki/ca.crt /etc/openvpn/
 cp /tmp/vpn/openvpn-certs/server/easyrsa3/pki/dh.pem /etc/openvpn/
 cp /tmp/vpn/openvpn-certs/server/easyrsa3/pki/issued/*.crt /etc/openvpn/
@@ -6,9 +10,6 @@ cp /tmp/vpn/openvpn-certs/server/easyrsa3/pki/private/*.crt /etc/openvpn/
 cd /etc/openvpn/
 openssl dhparam -out dh2048.pem 2048
 openvpn --genkey --secret ta.key
-wget -c https://raw.githubusercontent.com/mattock/mkinline/master/mkinline.sh
-chmod +x mkinline.sh
-echo 1 > /proc/sys/net/ipv4/ip_forward
 sysctl -p
-iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -j SNAT --to-source 10.0.0.4
+iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -j SNAT --to-source <local_IP>
 iptables -t nat -L -n -v
