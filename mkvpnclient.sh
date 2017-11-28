34bab560f394fc21d779908d614690885f5b5ce7
brianwrf <brianwangrewa@gmail.com>
2017-11-28 17:48:08 +0800

    Update mkvpnclient.sh

--- mkvpnclient.sh
+++ mkvpnclient.sh
@@ -1,6 +1,6 @@
 cd /tmp/vpn/openvpn-certs/server/easyrsa3
-./easyrsa build-client-full vpnclient
-cp /tmp/vpn/openvpn-certs/server/easyrsa3/pki/issued/vpnclient.crt /etc/openvpn/
-cp /tmp/vpn/openvpn-certs/server/easyrsa3/pki/private/vpnclient.key /etc/openvpn/
+./easyrsa build-client-full <vpnclient>
+cp /tmp/vpn/openvpn-certs/server/easyrsa3/pki/issued/<vpnclient>.crt /etc/openvpn/
+cp /tmp/vpn/openvpn-certs/server/easyrsa3/pki/private/<vpnclient>.key /etc/openvpn/
 cd /etc/openvpn/
 ./mkinline.sh -i client.conf -o <client_name>.ovpn
