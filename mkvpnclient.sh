3a1e2c2b403d3a50bb603d6a7aa3ca119e06f160
brianwrf <brianwangrewa@gmail.com>
2017-11-28 16:49:41 +0800

    Update mkvpnclient.sh

--- mkvpnclient.sh
+++ mkvpnclient.sh
@@ -3,7 +3,4 @@ cd /tmp/vpn/openvpn-certs/server/easyrsa3
 cp /tmp/vpn/openvpn-certs/server/easyrsa3/pki/issued/client.crt /etc/openvpn/
 cp /tmp/vpn/openvpn-certs/server/easyrsa3/pki/private/client.key /etc/openvpn/
 cd /etc/openvpn/
-git clone https://github.com/mattock/mkinline
-cp mkinline/mkinline.sh ./
-chmod +x mkinline.sh
 ./mkinline.sh -i client.conf -o <client_name>.ovpn
