2db08f260c5048e5b798e59d228630e65de525e9
brianwrf <brianwangrewa@gmail.com>
2017-12-05 16:47:15 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ tar -xvf lzo-2.09.tar.gz
 apt install libtool-bin
 libtool --finish /usr/local/lzo/lib
 apt-get install libpam0g-dev
+apt-get install libssl-dev
 cd /tmp/vpn/openvpn-2.3.8
 ./configure
 make 
