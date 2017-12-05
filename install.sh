2404780578aa98b5f62899a4e13712abb2afd5f2
brianwrf <brianwangrewa@gmail.com>
2017-12-05 16:52:02 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ apt install libtool-bin
 libtool --finish /usr/local/lzo/lib
 apt-get install libpam0g-dev
 apt-get install libssl-dev
+apt-get install liblzo2-dev
 cd /tmp/vpn/openvpn-2.3.8
 ./configure
 make 
